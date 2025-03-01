a69cea9ee5852876a165edd8c569e9f0dacaaab2
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-01 15:13:32 -0800

    [Saloni]Add Code of validating name and salary using custom exception

---
+++ employeepayrollapp/src/main/java/com/example/employeepayrollapp/ErrorResponseClass.java
@@ -0,0 +1,23 @@
+package com.example.employeepayrollapp;
+
+import java.util.List;
+
+    public class ErrorResponseClass {
+        private String message;
+        private List<String> details;
+
+        public ErrorResponseClass(String message, List<String> details) {
+            this.message = message;
+            this.details = details;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public List<String> getDetails() {
+            return details;
+        }
+    }
+
+
