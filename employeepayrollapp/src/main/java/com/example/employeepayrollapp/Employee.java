8a2567695efd576436e7d9bb9c994745a85ee6be
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-01 15:37:34 -0800

    [Saloni]Add Code of employee DTO adding new attribute

--- employeepayrollapp/src/main/java/com/example/employeepayrollapp/Employee.java
+++ employeepayrollapp/src/main/java/com/example/employeepayrollapp/Employee.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "employees")
@@ -17,5 +19,10 @@ public class Employee {
     private String name;
     @Min(value=500,message="Min Wage should be more than 500")
     private double salary;
+    public String gender;
+    public String startDate;
+    public String note;
+    public String profilePic;
+    public List<String> department;
 
 }
