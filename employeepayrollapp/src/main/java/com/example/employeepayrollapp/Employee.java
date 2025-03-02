c98439276e44b25bcdcedd05b433bedcc4db8eb9
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-01 16:30:16 -0800

    [Saloni]Add Code of adding data into database

--- employeepayrollapp/src/main/java/com/example/employeepayrollapp/Employee.java
+++ employeepayrollapp/src/main/java/com/example/employeepayrollapp/Employee.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.Data;
-
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,12 +14,14 @@ import java.util.List;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @Column(name ="employee_id")
+    public long id;
     @Pattern(regexp = "^[A-Za-z ]+$",message = "Name should contain alphabets and spaces")
     @NotNull(message="Employee name cannot be null")
-    private String name;
+    @Column(name="name")
+    public String name;
     @Min(value=500,message="Min Wage should be more than 500")
-    private double salary;
+    public double salary;
     @Pattern(regexp = "male|female", message = "Please enter valid gender")
     public String gender;
 
@@ -35,8 +36,31 @@ public class Employee {
 
     @NotBlank(message = "Profile Pic cannot be empty")
     public String profilePic;
-
+    @ElementCollection
+    @CollectionTable(name="employee_department",joinColumns = @JoinColumn(name="id"))
+    @Column(name="department")
     @NotEmpty(message = "Department cannot be empty")
     public List<String> department;
+    public Employee(Employee emp){
+        this.name=emp.name;
+        this.salary=emp.salary;
+        this.gender=emp.gender;
+        this.id=emp.id;
+        this.department=emp.department;
+        this.profilePic=emp.profilePic;
+        this.note=emp.note;
+        this.startDate=emp.startDate;
+    }
+    public Employee() {
+    }
 
+    public Employee(String name, double salary, String gender, LocalDate startDate, String note, String profilePic, List<String> department) {
+        this.name = name;
+        this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
+        this.note = note;
+        this.profilePic = profilePic;
+        this.department = department;
+    }
 }
