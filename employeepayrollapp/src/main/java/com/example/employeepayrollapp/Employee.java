c646d29dedd3ac9b8a04cf760b5b9feced7fe379
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-01 16:01:16 -0800

    [Saloni]Add code of validating attributes of employees

--- employeepayrollapp/src/main/java/com/example/employeepayrollapp/Employee.java
+++ employeepayrollapp/src/main/java/com/example/employeepayrollapp/Employee.java
@@ -1,11 +1,12 @@
 
 package com.example.employeepayrollapp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -15,14 +16,27 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Pattern(regexp = "^[A-Za-z ]+$",message = "Name should contain alphabets and spaces")
     @NotNull(message="Employee name cannot be null")
     private String name;
     @Min(value=500,message="Min Wage should be more than 500")
     private double salary;
+    @Pattern(regexp = "male|female", message = "Please enter valid gender")
     public String gender;
-    public String startDate;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @NotNull(message = "startDate should Not be Empty")
+    @PastOrPresent(message = "startDate should be past or today date")
+
+    public LocalDate startDate;
+
+    @NotBlank(message = "Note cannot be empty")
     public String note;
+
+    @NotBlank(message = "Profile Pic cannot be empty")
     public String profilePic;
+
+    @NotEmpty(message = "Department cannot be empty")
     public List<String> department;
 
 }
--- employeepayrollapp/src/main/java/com/example/employeepayrollapp/EmployeePayrollExceptionClass.java
+++ employeepayrollapp/src/main/java/com/example/employeepayrollapp/EmployeePayrollExceptionClass.java
@@ -1,15 +1,19 @@
+
 package com.example.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Slf4j
 @ControllerAdvice
 public class EmployeePayrollExceptionClass {
 
@@ -30,5 +34,14 @@ public class EmployeePayrollExceptionClass {
     public ResponseEntity<ErrorResponseClass> handleEmployeeId(EmployeeNotFound exception){
         ErrorResponseClass response=new ErrorResponseClass("Employee not found",List.of(exception.getMessage()));
         return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+    }
+    private static final List<String> message= new ArrayList<>();
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseClass> handleHttp(HttpMessageNotReadableException exception){
+        log.error("Invalid Date Formate",exception);
+        message.add("Exception while processing REST Request");
+        ErrorResponseClass responseClass=new ErrorResponseClass("Should have date in right formate",message ) ;
+        return new ResponseEntity<ErrorResponseClass>(responseClass,HttpStatus.BAD_REQUEST);
     }
 }
